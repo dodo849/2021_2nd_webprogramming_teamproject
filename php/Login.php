1c33c2cc7f3951eddbcb162236ccb21b1106b0a7
rlaalstn9907 <rlasmreo9907@gmail.com>
2021-11-21 20:03:38 +0900

    중간제출 미완

--- php/Login.php
+++ php/Login.php
@@ -1,22 +1,19 @@
-
-   
-<?php
+ <?php
 require_once("dbconfigID.php");
-
 $_POST = JSON_DECODE(file_get_contents("php://input"), true);
+//받아온 값을 변수에 넣는다
 $id = $_POST["id"];
 $pwd= $_POST["pwd"];
-
-$sql = "SELECT nickname FROM user WHERE id = '$id' and pwd='$pwd'";  
+//아이디와 비밀번호를 이용하여 닉네임을 찾는다.
+$sql = "SELECT nickname FROM user WHERE id = '$id' and pwd='$pwd'"; 
 $res = $db->query($sql); 
 $row = $res->fetch_array(MYSQLI_ASSOC);
-if ($row==!null) {
-    echo json_encode($row,JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
-    session_start();
-    $_SESSION['User_Name']=$row['nickname'];
+if ($row==!NULL) {//row에 값이 있다, 즉 아이디와 비밀번호로 성공적으로 닉네임을 찾았을 경우 실행 
+    session_start();//세션 시작
+    $_SESSION['User_Name']=$row['nickname'];//닉네임을 세션변수 $_SESSION['User_Name']에 저장
     header('Location: /html/mypage.html');
+    echo true;
 } else {            
-    echo("fail");
     echo false;   
 }
 mysqli_close($db);
--- php/mypage.php
+++ php/mypage.php
@@ -1,6 +1,7 @@
 <?php
 require_once("dbconfigID.php");
 session_start();
+//세션 변수의 값 , 닉네임을 받아온다.
 if($_SESSION['User_Name']){
     echo($_SESSION['User_Name']);
 }

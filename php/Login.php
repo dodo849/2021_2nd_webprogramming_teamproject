757d99eb0906712b04063b2a979d5a4bd98a8d69
doyeon <do8343@naver.com>
2021-11-21 11:46:02 +0900

    로그인 파일 복구

--- php/Login.php
+++ php/Login.php
@@ -1,3 +1,5 @@
+
+   
 <?php
 require_once("dbconfigID.php");
 
@@ -8,13 +10,11 @@ $pwd= $_POST["pwd"];
 $sql = "SELECT nickname FROM user WHERE id = '$id' and pwd='$pwd'";  
 $res = $db->query($sql); 
 $row = $res->fetch_array(MYSQLI_ASSOC);
-
 if ($row==!null) {
     echo json_encode($row,JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
-    echo("로그인성공");
     session_start();
     $_SESSION['User_Name']=$row['nickname'];
-    echo $_SESSION['User_Name'];
+    header('Location: /html/mypage.html');
 } else {            
     echo("fail");
     echo false;   

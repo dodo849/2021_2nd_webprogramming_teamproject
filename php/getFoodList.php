6af2f7586809f64a59544bbc73fb2d553959631d
doyeon <do8343@naver.com>
2021-11-23 18:44:39 +0900

    주석 작성 완료 :

--- php/getFoodList.php
+++ php/getFoodList.php
@@ -4,8 +4,10 @@
 
     $_POST = JSON_DECODE(file_get_contents("php://input"), true);
 
+    // 선택된 음식 분류 (한식, 양식, 중식, 일식, 패스트푸드, 디저트 중 1개)를 js에서 넘겨 받는다.
     $selectedFoodType = $_POST["selectedFoodType"];
 
+    //해당 분류를 가진 음식 레코드를 모두 가져온다.
     $sql = "SELECT * FROM food WHERE food_type='$selectedFoodType';"; //sql 구문 넣기
 
     $data = array();
